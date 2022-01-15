02dc64c67cc81005db6bb9947890b3813c8705b5
julie-chernen <juliechernen@gmail.com>
2022-01-15 18:02:29 +0200

    ready

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 
 const {
   loadData,
-  checkUserPresence,
   saveData,
-  checkUser,
+  checkUserToAdd,
+  calcutaleWithdraw,
+  calculateTransfer,
+  checkActive,
 } = require("./utils.js");
 
 app.use(express.json());
@@ -19,12 +21,12 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const data = loadData();
   const newUser = req.body;
-  if (checkUser(newUser)) {
+  if (checkUserToAdd(newUser)) {
     data.push(newUser);
     saveData(data);
     res.status(201).send(JSON.stringify(data));
   } else {
-    res.send("send unique user with full and correct data");
+    res.status(400).send("send unique user with full and correct data");
   }
 });
 
@@ -34,27 +36,128 @@ app.put("/cash", (req, res) => {
   const userIdToEdit = req.query.userId;
   const cash = req.body.cash;
   const editIndex = data.findIndex((user) => user.pasportId == userIdToEdit);
-  if (editIndex > -1) {
-    data[editIndex].cash = data[editIndex].cash + cash;
+  const user = data[editIndex];
+  if (editIndex === -1) {
+    res.status(400).send(`The user with id ${userIdToEdit} doesn't exist`);
+  } else if (!checkActive(user)) {
+    res
+      .status(400)
+      .send(
+        `The user with id ${userIdToEdit} is not active, operations are unsupported`
+      );
+  } else {
+    user.cash = user.cash + cash;
     saveData(data);
     res.status(200).send(JSON.stringify(data));
-  } else {
-    res.send("enter cash to user that exists");
   }
 });
 
-//try to add credit
+// to add credit
 app.put("/credit", (req, res) => {
   const data = loadData();
   const userIdToEdit = req.query.userId;
   const credit = req.body.credit;
   const editIndex = data.findIndex((user) => user.pasportId == userIdToEdit);
-  if (editIndex > -1) {
-    data[editIndex].credit = data[editIndex].credit + credit;
+
+  const user = data[editIndex];
+  if (editIndex === -1) {
+    res.status(400).send(`The user with id ${userIdToEdit} doesn't exist`);
+  } else if (!checkActive(user)) {
+    res
+      .status(400)
+      .send(
+        `The user with id ${userIdToEdit} is not active, operations are unsupported`
+      );
+  } else {
+    user.credit = user.credit + credit;
     saveData(data);
     res.status(200).send(JSON.stringify(data));
+  }
+});
+
+//withdraw money
+app.put("/withdraw", (req, res) => {
+  const data = loadData();
+  const id = req.query.userId;
+  const withdraw = req.body.withdraw;
+  const index = data.findIndex((dataUser) => dataUser.pasportId == id);
+  let user = data[index];
+  if (index === -1) {
+    res.status(400).send(`the user with id ${id} doesn't exist`);
+  } else if (!checkActive(user)) {
+    res
+      .status(400)
+      .send(`the user with id ${id} is not active, operations are unsuported`);
   } else {
-    res.send("enter credit to user that exists");
+    user = calcutaleWithdraw(withdraw, user);
+    if (user) {
+      saveData(data);
+      res.status(200).send(JSON.stringify(data));
+    } else {
+      res
+        .status(400)
+        .send(
+          `the user with id ${id} doesn't have enough money for withdraw ${withdraw}`
+        );
+    }
+  }
+});
+
+//transfer money
+app.put("/transfer", (req, res) => {
+  let data = loadData();
+  const idUserFrom = req.query.idUserFrom;
+  const idUserTo = req.query.idUserTo;
+  const transfer = req.body.transfer;
+  const indexFrom = data.findIndex(
+    (dataUser) => dataUser.pasportId == idUserFrom
+  );
+  const indexTo = data.findIndex((dataUser) => dataUser.pasportId == idUserTo);
+
+  let userFrom = data[indexFrom];
+  let userTo = data[indexTo];
+  if (indexFrom === -1 || indexTo === -1) {
+    if (indexFrom === -1 && indexTo === -1) {
+      res
+        .status(400)
+        .send(`The users with id ${idUserFrom} and id ${idUserTo} don't exist`);
+    } else if (indexTo === -1) {
+      res.status(400).send(`The user with id ${idUserTo} doesn't exist`);
+    } else {
+      res.status(400).send(`The user with id ${idUserFrom} doesn't exist`);
+    }
+  } else if (!checkActive(userFrom) || !checkActive(userTo)) {
+    if (!checkActive(userFrom) && !checkActive(userTo)) {
+      res
+        .status(400)
+        .send(
+          `The user with id ${idUserFrom} and id ${idUserTo} are not active, operations are unsupported`
+        );
+    } else if (!checkActive(userFrom)) {
+      res
+        .status(400)
+        .send(
+          `The user with id ${idUserFrom} is not active, operations are unsupported`
+        );
+    } else {
+      res
+        .status(400)
+        .send(
+          `The user with id ${idUserTo} is not active, operations are unsupported`
+        );
+    }
+  } else {
+    newData = calculateTransfer(transfer, data, indexFrom, indexTo);
+    if (newData) {
+      saveData(newData);
+      res.status(200).send(JSON.stringify(newData));
+    } else {
+      res
+        .status(400)
+        .send(
+          `The user with id ${idUserFrom} doesn't have enough money for transfer ${transfer}`
+        );
+    }
   }
 });
 
@@ -62,11 +165,40 @@ app.put("/credit", (req, res) => {
 app.get("/users", (req, res) => {
   const data = loadData();
   const id = req.query.userId;
-  if (checkUserPresence(id)) {
-    const index = data.findIndex((dataUser) => dataUser.pasportId == id);
+  const index = data.findIndex((dataUser) => dataUser.pasportId == id);
+  if (index > -1) {
     res.status(200).send(JSON.stringify(data[index]));
   } else {
-    res.send("the user doesn't exist");
+    res.send(`The user with id ${id} doesn't exist`);
+  }
+});
+
+//get user info by amount of cash
+app.get("/cash", (req, res) => {
+  const data = loadData();
+  const cash = req.query.cash;
+  const cashResult = data.filter((dataUser) => dataUser.cash === cash);
+  if (cashResult.length > 0) {
+    res.status(200).send(JSON.stringify(cashResult));
+  } else {
+    res.status(400).send(`The user with cash ${cash} not found`);
+  }
+});
+
+//get users that are active and have more than X amount of money in cash
+app.get("/cash-active", (req, res) => {
+  const data = loadData();
+  const cash = req.query.cash;
+
+  const users = data.filter(
+    (dataUser) => dataUser.cash >= cash && dataUser.isActive === true
+  );
+  if (users.length > 0) {
+    res.status(200).send(JSON.stringify(users));
+  } else {
+    res
+      .status(400)
+      .send(`No active users are found with cash equal or more ${cash}`);
   }
 });
 
--- utils.js
+++ utils.js
@@ -15,20 +15,9 @@ const saveData = (data) => {
   fs.writeFileSync("./db/data.json", JSON.stringify(data));
 };
 
-const checkUserPresence = (id) => {
-  const data = loadData();
-  result = false;
-  data.forEach((dataUser) => {
-    if (dataUser.pasportId == id) {
-      result = true;
-    }
-  });
-  return result;
-};
-
-const checkUser = (user) => {
+const checkUserToAdd = (user) => {
   const checkKeys = Object.keys(user).includes(
-    "pasportId" && "cash" && "credit"
+    "pasportId" && "cash" && "credit" && "isActive"
   );
 
   let repeatCheck = true;
@@ -39,9 +28,53 @@ const checkUser = (user) => {
     }
   });
   //pasportId may be number or string
-  const [passwordId, cash, credit] = Object.values(user);
-  const checkValues = typeof cash === "number" || typeof credit !== "number";
+  const [passwordId, cash, credit, isActive] = Object.values(user);
+  const checkValues =
+    typeof cash === "number" &&
+    typeof credit === "number" &&
+    (isActive === false || isActive === true);
   return checkKeys && repeatCheck && checkValues;
 };
 
-module.exports = { loadData, saveData, checkUser, checkUserPresence };
+const calcutaleWithdraw = (withdrawSum, object) => {
+  if (withdrawSum <= object.cash + object.credit) {
+    if (withdrawSum <= object.cash) {
+      object.cash = object.cash - withdrawSum;
+    } else if (withdrawSum > object.cash) {
+      object.credit = object.credit + object.cash - withdrawSum;
+      object.cash = 0;
+    }
+    return object;
+  } else {
+    return null;
+  }
+};
+
+const calculateTransfer = (transferSum, data, indexFrom, indexTo) => {
+  const objectFrom = data[indexFrom];
+  const objectTo = data[indexTo];
+  if (calcutaleWithdraw(transferSum, objectFrom)) {
+    calcutaleWithdraw(transferSum, objectFrom);
+    objectTo.cash += transferSum;
+    return data;
+  } else {
+    return null;
+  }
+};
+
+const checkActive = (user) => {
+  if (user.isActive === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = {
+  loadData,
+  saveData,
+  checkUserToAdd,
+  calcutaleWithdraw,
+  calculateTransfer,
+  checkActive,
+};

3b1d5f277293599623552c3dd94e679b8260d58d
Toam10 <78689759+Toam10@users.noreply.github.com>
2022-01-17 01:03:10 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,8 +6,15 @@ const loadData = () => {
     const dataJSON = dataBuffer.toString();
     const data = JSON.parse(dataJSON);
     return data;
+// you can do this in one line look at this:
+   // loadData = () => JSON.parse(fs.readFileSync("./db/data.json","utf-8))
+    
   } catch (e) {
     return [];
+    // Meaningless 
+    // use throw Error("some text will be better")
+    //maybe gnarate a new file if there is not a file of json with some data
+    // dont use try and catch inside a utils if this is sync and most of the time we do sync things inside utils
   }
 };
 
@@ -15,10 +22,13 @@ const saveData = (data) => {
   fs.writeFileSync("./db/data.json", JSON.stringify(data));
 };
 
-const checkUserToAdd = (user) => {
-  const checkKeys = Object.keys(user).includes(
-    "pasportId" && "cash" && "credit" && "isActive"
-  );
+// good dont do it in one line because this function isnt return something and that is clearer
+
+const checkUserToAdd = (user) => Object.keys(user).includes("pasportId" && "cash" && "credit" && "isActive")
+    // make a specific function for this  "pasportId" && "cash" && "credit" && "isActive" that return true or false
+    // nice using object.keys we see it alot in the wild
+    // one line please that name of the function is tell us the story
+  
 
   let repeatCheck = true;
   const data = loadData();
@@ -28,13 +38,15 @@ const checkUserToAdd = (user) => {
     }
   });
   //pasportId may be number or string
-  console.log(user);
+  console.log(user); // remove console.log()
   const [pasportId, cash, credit, isActive] = Object.values(user);
+  // nice this is a nice way of using Object.values but
+
   const checkValues =
     typeof cash === "number" &&
     typeof credit === "number" &&
     (isActive === "false" || isActive === "true");
-
+// remove as other util function
   return checkKeys && repeatCheck && checkValues;
 };
 
@@ -47,6 +59,7 @@ const calculateWithdraw = (withdrawSum, object) => {
       object.cash = 0;
     }
     return object;
+    // function name: calculateWithdraw is not a good name if you return object in the end you need to think what you take (args) what you do (somecode) and what you return (return value) !important
   } else {
     return null;
   }
@@ -55,10 +68,14 @@ const calculateWithdraw = (withdrawSum, object) => {
 const calculateTransfer = (transferSum, data, indexFrom, indexTo) => {
   const objectFrom = data[indexFrom];
   const objectTo = data[indexTo];
+  // that a user not only a object if we know that users is array of docs we dont need that you tell us that its a object call is user
+  
   if (calculateWithdraw(transferSum, objectFrom)) {
     calculateWithdraw(transferSum, objectFrom);
     objectTo.cash += transferSum;
     return data;
+    // 
+     // function name: calculateTransfer is not a good name if you return object in the end you need to think what you take (args) what you do (somecode) and what you return (return value) !important
   } else {
     return null;
   }
